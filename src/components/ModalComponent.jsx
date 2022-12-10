0e84a6880ddbfac7f765db6119a5661d15fece29
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-12-09 23:19:14 -0300

    Importando os componentes de Modal do Chakra-ui

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,4 +1,21 @@
-const ModelComponent = () => {
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const ModelComponent = ({ data, setdata, dataEdit, isOpen, onClose }) => {
+  
   return (
     <></>
   );
